05a655bcafa4541449372f4f4e25ba67d1e9980b
Echo <github@3kh0.net>
2025-07-21 14:49:12 -0400

    update amount

--- server/api/udx.get.ts
+++ server/api/udx.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const total = 1115;
+  const total = 1454;
   // we will add some cooler shit here later
   return {
     t: total.toLocaleString(),
