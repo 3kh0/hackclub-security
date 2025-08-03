6a3f1518778d60b0b9a319fdca2596dfc63ebc65
Echo <github@3kh0.net>
2025-08-03 02:28:25 -0400

    rebase

--- server/api/udx.get.ts
+++ server/api/udx.get.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler(async (event) => {
-  const total = 1854;
+  const total = 1954;
   // we will add some cooler shit here later
   // update, we did not add the cool shit, this is manually updated lmao
   return {
